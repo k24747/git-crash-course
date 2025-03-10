2080b7c8509699e246fcfd9b2f71a767dbced260
Kavya Khandelwal <kavya.k@intimetec.com>
2025-03-10 14:22:45 +0530

    Subtract

--- file.cpp
+++ file.cpp
@@ -7,6 +7,12 @@ int add(int a, int b){
     return a+b;
 }
 
+int sub(int a, int b){
+    cout<<"New Feature"<<endl;
+    cout<<"Second New Feature"<<endl;
+    return a-b;
+}
+
 int main(){
     //int num = add(5, 2);
     //cout << num << endl;
