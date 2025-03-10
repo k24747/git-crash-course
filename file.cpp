3ea210fabb2d2840d8224bc0c48775a78b936cb9
Kavya Khandelwal <kavya.k@intimetec.com>
2025-03-10 11:19:49 +0530

    Added full code

--- file.cpp
+++ file.cpp
@@ -1,9 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
 
 int add(int a, int b){
     return a+b;
 }
 
 int main(){
-    add(5, 2);
+    int num = add(5, 2);
+    cout << num << endl;
     return 0;
 }
