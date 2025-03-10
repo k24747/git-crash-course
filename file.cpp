cdb1fa239efb0da89b7756290ddfca6b09e5e5fa
Kavya Khandelwal <kavya.k@intimetec.com>
2025-03-10 11:44:45 +0530

    Removed add function

--- file.cpp
+++ file.cpp
@@ -1,12 +1,12 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int add(int a, int b){
-    return a+b;
-}
+// int add(int a, int b){
+//     return a+b;
+// }
 
 int main(){
-    int num = add(5, 2);
-    cout << num << endl;
+    //int num = add(5, 2);
+    //cout << num << endl;
     return 0;
 }
